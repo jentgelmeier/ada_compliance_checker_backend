e25397620ee9270ff2a4a6969b3e01d198a49395
Jason Entgelmeier <jasonentgelmeier@Emilys-MacBook-Pro-2.local>
2025-09-03 11:52:01 -0400

    add another route for checking html fro m urls

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import cross_origin
+import requests
 
 from ada_checks import check_h1, check_headers, check_lang, check_title, check_img_alt, check_link_text
 from contrast_check import check_contrast_ratio
@@ -8,34 +9,8 @@ from contrast_check import check_contrast_ratio
 # The __name__ variable helps Flask find the root path of the application
 app = Flask(__name__)
 
-# Define an API endpoint for the root URL ('/')
-# This endpoint will respond to GET requests.
-@app.route('/')
-def home():
-    """
-    Handles requests to the home page.
-    Returns a simple greeting message.
-    """
-    return "<h1>Welcome to the Flask API!</h1><p>Try /api/v1/ada-check to check if your html meets accessibility standards.</p>"
-
-# This endpoint will respond to POST requests to '/api/v1/ada-check'.
-@app.route('/api/v1/ada-check', methods=['POST'])
-@cross_origin()
-def check_html_accessibility():
-    """
-    Receives a JSON object with an 'html' string and returns
-    Expects a JSON payload like: {"html": "your string here"}.
-    Returns a JSON response: [{"problem": "Low Contrast Ratio", "element": "<h1>" , "details": "The contrast ratio is 1.98. The
-    minimum required for large text is 3.0.", "rule": ""COLOR_CONTRAST"}, {...}].
-    """
-    request_data = request.get_json()
-
-    # Validate that the request_data is not empty and contains the 'text' key.
-    if not request_data or 'html' not in request_data:
-        return jsonify({"message": "Invalid request: JSON object with 'html' key required"}), 400
-
-    # Ensure the value associated with 'text' is actually a string.
-    input_string = request_data['html']
+def check_html_accessibility(input_string):
+    # Ensure the input_string is actually a string.
     if not isinstance(input_string, str):
         return jsonify({"message": "Invalid input: 'html' must be a string"}), 400
 
@@ -78,6 +53,58 @@ def check_html_accessibility():
 
     # Return the result as a JSON object.
     return jsonify(response), 200
+
+# Define an API endpoint for the root URL ('/')
+# This endpoint will respond to GET requests.
+@app.route('/')
+def home():
+    """
+    Handles requests to the home page.
+    Returns a simple greeting message.
+    """
+    return "<h1>Welcome to the Flask API!</h1><p>Post a string of html to /api/v1/html-check or a url to /api/v1/url-check to check if your html meets accessibility standards.</p>"
+
+# This endpoint will respond to POST requests to '/api/v1/html-check'.
+@app.route('/api/v1/html-check', methods=['POST'])
+@cross_origin()
+def check_string():
+    """
+    Expects a JSON payload like: {"html": "your string here"}.
+    Returns a JSON response: [{"problem": "Low Contrast Ratio", "element": "<h1>" , "details": "The contrast ratio is 1.98. The
+    minimum required for large text is 3.0.", "rule": ""COLOR_CONTRAST"}, {...}].
+    """
+    request_data = request.get_json()
+
+    # Validate that the request_data is not empty and contains the 'html' key.
+    if not request_data or 'html' not in request_data:
+        return jsonify({"message": "Invalid request: JSON object with 'html' key required"}), 400
+
+    input_string = request_data['html']
+    
+    return check_html_accessibility(input_string)
+    
+# This endpoint will respond to POST requests to '/api/v1/url-check'.
+@app.route('/api/v1/url-check', methods=['POST'])
+@cross_origin()
+def check_url():
+    """
+    Expects a JSON payload like: {"url": "your url here"}.
+    Returns a JSON response: [{"problem": "Low Contrast Ratio", "element": "<h1>" , "details": "The contrast ratio is 1.98. The
+    minimum required for large text is 3.0.", "rule": ""COLOR_CONTRAST"}, {...}].
+    """
+
+    request_data = request.get_json()
+
+    # Validate that the request_data is not empty and contains the 'url' key.
+    if not request_data or 'url' not in request_data:
+        return jsonify({"message": "Invalid request: JSON object with 'url' key required"}), 400
+    
+    # Grab the html from the provided url
+    url = request_data['url']
+    response = requests.get(url)
+    input_string = response.text
+
+    return check_html_accessibility(input_string)
 
 # This block ensures the Flask development server runs only when the script is executed directly.
 if __name__ == '__main__':
